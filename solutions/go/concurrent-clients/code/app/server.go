e3ef8b0cfc45e46f2349b74698400ac9b4817104
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-09 18:03:18 +0100

    add more solutions

--- solutions/go/concurrent-clients/code/app/server.go
+++ solutions/go/concurrent-clients/code/app/server.go
@@ -13,16 +13,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	if _, err := conn.Read([]byte{}); err != nil {
-		fmt.Println("error reading from client: ", err.Error())
-		os.Exit(1)
+		go handleConnection(conn)
 	}
+}
 
-	conn.Write([]byte("+PONG\r\n"))
+func handleConnection(conn net.Conn) {
+	for {
+		if _, err := conn.Read([]byte{}); err != nil {
+			fmt.Println("Error reading from client: ", err.Error())
+			continue
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
