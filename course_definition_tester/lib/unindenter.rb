751c85547e45494ca6ac20f3abb33ed65c0d30a4
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-06-09 16:28:16 +0100

    handle further indented lines

--- course_definition_tester/lib/unindenter.rb
+++ course_definition_tester/lib/unindenter.rb
@@ -1,13 +1,20 @@
 class Unindenter
   def self.unindent(string)
-    largest_tab_indent = string.scan(/^\t*(?=\S)/).map(&:length).max
-    largest_space_indent = string.scan(/^ *(?=\S)/).map(&:length).max
+    smallest_tab_indent = string.scan(/^\t*(?=\S)/).map(&:length).max
+    smallest_space_indent = string.scan(/^ *(?=\S)/).map(&:length).max
 
     string
       .lines
       .map { |line|
-        line[(largest_space_indent || largest_tab_indent || 0)..-1]&.strip || ""
+        if smallest_tab_indent
+          line.sub(/^\t{#{smallest_tab_indent}}/, "")
+        elsif smallest_space_indent
+          line.sub(/^ {#{smallest_space_indent}}/, "")
+        else
+          line
+        end
       }
+      .map(&:rstrip)
       .join("\n")
   end
 end
