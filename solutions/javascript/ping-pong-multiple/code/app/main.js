3ad9733d6107f61d8703d8f90f81427e8392107d
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-22 01:17:47 +0100

    Remove existing solutions for javascript-ping-pong-multiple

--- solutions/javascript/ping-pong-multiple/code/app/main.js
+++
@@ -1,9 +0,0 @@
-const net = require("net");
-
-const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("+PONG\r\n");
-  });
-});
-
-server.listen(6379, "127.0.0.1");
