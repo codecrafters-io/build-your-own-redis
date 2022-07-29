b64e700333a96d7a813c2a242120c0a8b9894fc8
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-07-29 12:12:32 +0800

    add conn.Close(), remove conn.Read() from stage 2

--- solutions/go/ping-pong/code/app/server.go
+++ solutions/go/ping-pong/code/app/server.go
@@ -19,10 +19,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if _, err := conn.Read([]byte{}); err != nil {
-		fmt.Println("error reading from client: ", err.Error())
-		os.Exit(1)
-	}
+	defer conn.Close()
 
 	conn.Write([]byte("+PONG\r\n"))
 }
