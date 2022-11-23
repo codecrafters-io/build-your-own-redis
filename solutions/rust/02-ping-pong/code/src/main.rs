028bc425dc58c984cbe374f6ec90abbfa51dcac8
ThetaSinner <greatdjonfire@hotmail.co.uk>
2022-11-23 22:05:46 +0000

    Update rust step 2 and add missing explanation

--- solutions/rust/02-ping-pong/code/src/main.rs
+++ solutions/rust/02-ping-pong/code/src/main.rs
@@ -1,5 +1,6 @@
-use std::io::Write;
 use std::net::TcpListener;
+use bytes::BytesMut;
+use std::io::{Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -8,6 +9,11 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
+
+                // Wait for the client to send us a message but ignore the content for now
+                let mut buf = BytesMut::with_capacity(512);
+                stream.read(&mut buf).unwrap();
+
                 stream.write("+PONG\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
