6296ab1e6726ec7e67b20d3dba931dab85aaee61
ThetaSinner <greatdjonfire@hotmail.co.uk>
2022-11-23 22:19:02 +0000

    Add rust step 3

---
+++ solutions/rust/03-ping-pong-multiple/code/src/main.rs
@@ -0,0 +1,30 @@
+use std::net::TcpListener;
+use bytes::BytesMut;
+use std::io::{Read, Write};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+
+                loop {
+                    // Wait for the client to send us a message but ignore the content for now
+                    let mut buf = BytesMut::with_capacity(512);
+                    let bytes_read = stream.read(&mut buf).unwrap();
+                    if bytes_read == 0 {
+                        println!("client closed the connection");
+                        break;
+                    }
+
+                    stream.write("+PONG\r\n".as_bytes()).unwrap();
+                }
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
