0fa21f5bf03d943ff4577790a9e3384f27317bcf
libmartinito <lib.martinito@pm.me>
2023-11-04 15:23:59 +0800

    CC-810 updated naming

--- starter_templates/java/spawn_redis_server.sh
+++ starter_templates/java/spawn_redis_server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 mvn -B --quiet package -Ddir=/tmp/codecrafters-redis-target
-exec java -jar /tmp/codecrafters-bittorrent-target/redis_bittorrent.jar "$@"
+exec java -jar /tmp/codecrafters-redis-target/java_redis.jar "$@"
