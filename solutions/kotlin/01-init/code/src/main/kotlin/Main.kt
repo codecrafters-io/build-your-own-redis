9df77410895ee55ec01d525c2b88d1a1b23848ea
Uriel Salischiker <uriels@spotify.com>
2023-11-16 14:27:39 +0100

    Add support for Kotlin

---
+++ solutions/kotlin/01-init/code/src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+import java.io.IOException
+import java.net.ServerSocket
+import java.net.Socket
+
+fun main(args: Array<String>) {
+    val serverSocket: ServerSocket
+    val clientSocket: Socket
+    val port = 6379
+    try {
+        serverSocket = ServerSocket(port)
+        serverSocket.reuseAddress = true
+        // Wait for connection from client.
+        clientSocket = serverSocket.accept()
+    } catch (e: IOException) {
+        println("IOException: " + e.message)
+    } finally {
+        try {
+            if (clientSocket != null) {
+                clientSocket.close()
+            }
+        } catch (e: IOException) {
+            println("IOException: " + e.message)
+        }
+    }
+}
