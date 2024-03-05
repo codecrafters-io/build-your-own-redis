073884120dac5d5951ffab61c22967ff7c0838e5
Ryan Gang <ryan-gg@outlook.com>
2024-03-05 22:08:34 +0530

    CC-1030: Replace uses of SO_REUSEPORT with SO_REUSEADDR.

--- starter_templates/csharp/src/Server.cs
+++ starter_templates/csharp/src/Server.cs
@@ -6,5 +6,8 @@ Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 // TcpListener server = new TcpListener(IPAddress.Any, 6379);
+// // Since the tester restarts your program quite often, setting SO_REUSEADDR
+// // ensures that we don't run into 'Address already in use' errors
+// server.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
 // server.Start();
 // server.AcceptSocket(); // wait for client
--- starter_templates/java/src/main/java/Main.java
+++ starter_templates/java/src/main/java/Main.java
@@ -13,6 +13,8 @@ public class Main {
     //    int port = 6379;
     //    try {
     //      serverSocket = new ServerSocket(port);
+    //      // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    //      // ensures that we don't run into 'Address already in use' errors
     //      serverSocket.setReuseAddress(true);
     //      // Wait for connection from client.
     //      clientSocket = serverSocket.accept();
