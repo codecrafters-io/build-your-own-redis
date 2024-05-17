c12d3f7dd205106f11adb2ef90dd69b2bac72aae
Ryan <ryan-gg@outlook.com>
2024-05-17 18:59:40 +0000

    feat: add caching to clojure builds

---
+++ dockerfiles/clojure-1.11.3.Dockerfile
@@ -0,0 +1,14 @@
+FROM clojure:openjdk-18-lein-alpine
+
+ENV CODECRAFTERS_DEPENDENCY_FILE_PATHS="project.clj"
+
+WORKDIR /app
+COPY project.clj ./
+
+RUN lein deps
+
+RUN mkdir -p /app-cached/deps/
+RUN if [ -d "/app/deps/" ]; then mv /app/deps/ /app-cached/deps/; fi
+
+RUN echo "cd \${CODECRAFTERS_SUBMISSION_DIR} && lein deps" > /codecrafters-precompile.sh
+RUN chmod +x /codecrafters-precompile.sh
--- solutions/clojure/01-jm1/code/project.clj
+++ solutions/clojure/01-jm1/code/project.clj
@@ -3,8 +3,12 @@
   :url "http://github.com/codecrafters-io/redis-starter-clojure"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.cli "1.1.230"]
+                 [aleph "0.6.4"]]
+
   :main ^:skip-aot redis.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+            :user {:local-repo "/app/deps/"}})
