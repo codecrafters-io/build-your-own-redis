227e9385dc4bd1a52e5d325197f19b192a4ef672
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-16 13:53:59 +0100

    scala

--- compiled_starters/scala/your_program.sh
+++ compiled_starters/scala/your_program.sh
@@ -8,9 +8,14 @@
 
 set -e # Exit early if any commands fail
 
+# Copied from .codecrafters/compile.sh
+#
+# - Edit this to change how your program compiles locally
+# - Edit .codecrafters/compile.sh to change how your program compiles remotely
+sbt assembly
+
 # Copied from .codecrafters/run.sh
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-sbt assembly
 exec java -jar ./target/scala-2.13/redis.jar "$@"
