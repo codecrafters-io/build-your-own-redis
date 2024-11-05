0175a7347f9f462ccf0f7ff294a2cbd00b108a49
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-11-05 08:28:47 +0800

    Disable buffering for Haskell

--- solutions/haskell/01-jm1/code/app/Main.hs
+++ solutions/haskell/01-jm1/code/app/Main.hs
@@ -5,10 +5,14 @@
 module Main (main) where
 
 import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock)
-
+import System.IO (hSetBuffering, stdout, stderr, BufferMode(NoBuffering))
 
 main :: IO ()
 main = do
+    -- Disable output buffering
+    hSetBuffering stdout NoBuffering
+    hSetBuffering stderr NoBuffering
+
     let port = "6379"
     putStrLn $ "Redis server listening on port " ++ port
     serve HostAny port $ \(socket, address) -> do
