65bf06dc2da717e20d9c584911d80d9b74242979
Andy Li <1450947+andy1li@users.noreply.github.com>
2026-04-07 05:41:53 +0900

    Update Scala project configuration and build process
    - Changed the buildpack version in `codecrafters.yml` from Scala 3.7 to 3.8.
    - Updated the installation instructions in `README.md` to require `scala-cli` instead of `sbt`.
    - Modified the build and run scripts to use `scala-cli` for packaging and execution.
    - Removed obsolete `assembly.sbt` and `build.properties` files.
    - Added a new Dockerfile for Scala 3.8.
    - Updated `.gitignore` files across various directories to include additional IDE and build artifacts.

--- solutions/scala/02-rg2/code/your_program.sh
+++ solutions/scala/02-rg2/code/your_program.sh
@@ -14,11 +14,13 @@ set -e # Exit early if any commands fail
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  SBT_OPTS="--enable-native-access=ALL-UNNAMED" sbt assembly
+  scala-cli package src/main/scala/ \
+    -q --power --assembly --force --server=false --scala-version=3.8.3 \
+    -o /tmp/codecrafters-build-redis-scala
 )
 
 # Copied from .codecrafters/run.sh
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec java -jar ./target/scala-3.7.1/redis.jar "$@"
+exec java -jar /tmp/codecrafters-build-redis-scala "$@"
